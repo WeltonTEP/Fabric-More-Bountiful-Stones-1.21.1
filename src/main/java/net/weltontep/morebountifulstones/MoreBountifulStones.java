ecfd02f5b85edf8a55cb3d845df9356db62a7871
WeltonTEP <WeltonTEP@gmail.com>
2024-09-19 22:34:57 -0700

    a

--- src/main/java/net/weltontep/morebountifulstones/MoreBountifulStones.java
+++ src/main/java/net/weltontep/morebountifulstones/MoreBountifulStones.java
@@ -2,12 +2,14 @@ package net.weltontep.morebountifulstones;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.weltontep.morebountifulstones.block.ModBlocks;
 import net.weltontep.morebountifulstones.item.ModItems;
+import net.weltontep.morebountifulstones.item.ModItemGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MoreBountifulStones implements ModInitializer {
-	public static final String MOD_ID = "MoreBountifulStones";
+	public static final String MOD_ID = "morebountifulstones";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -16,7 +18,10 @@ public class MoreBountifulStones implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
